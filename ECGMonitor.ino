e6d2fc366ebb39a98e6245f900b88362d7135a02
Jawad Hoballah <jawad.hoballah@duke.edu>
2017-12-12 15:58:44 +0200

    Updated .ino file to include pause functionality. Commented analogECG functions and setups for improved readability.

--- ECGMonitor.ino
+++ ECGMonitor.ino
@@ -16,6 +16,7 @@ int ledState = LOW;
 int analog_beat_counter = 0;
 int analog_inst_hr_count = 0;
 
+unsigned long currentAnalogMillis = 0;
 unsigned long previousTime = 0;
 unsigned long startTime = 0;
 unsigned long tachyInterval = 50;
@@ -27,12 +28,10 @@ float ecgAnalogSignal = 0;
 float voltage = 0;
 float analog_threshold = 2.0;
 
+const byte pausePin = 2;
+volatile byte pauseState = LOW;
 
-
-const byte interruptPin = 2;
-volatile byte state = LOW;
-
-
+// holds the analog_avg_hr using the Average library
 Average<float> analog_avg_hr(rolling_average_size);
 
 
@@ -41,12 +40,21 @@ void setup() {
 
   ledSetup();
   signalReadSetup();
+  
+  // add pause interrupt
+  attachInterrupt(digitalPinToInterrupt(pausePin), pause, CHANGE);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (pauseState != HIGH){
+    readECGAnalog();
+    analogHRMonitor(currentAnalogMillis);
+  }
+  else {
+    
+  }
 }
 
 void ledSetup() {
@@ -55,36 +63,47 @@ void ledSetup() {
 }
 
 void signalReadSetup() {
-
+  // Prepare pins as inputs for analog and digital signals
   pinMode(ecgAnalogSignalPin, INPUT);
   pinMode(ecgDigitalSignalPin, INPUT);
 }
 
 void readECGAnalog() {
-
+  // read analog signal from analog signal pin
   ecgAnalogSignal = analogRead(ecgAnalogSignalPin);
   voltage = MAXVOLT*ecgAnalogSignal/1023;
-  unsigned long currentAnalogMillis = millis();
+  //begin timer
+  currentAnalogMillis = millis();
+  Serial.println(voltage);
   
 }
 
 
 void analogHRMonitor(unsigned long currentAnalogMillis) {
+  // check if voltage is above a specific threshold, can be modified from analog_threshold var at top of code
   if (voltage > analog_threshold) {
-  delay(250);
-  analog_beat_counter++;
-  float analog_interval_hr[analog_beat_counter] = {1./ ((currentAnalogMillis - previousTime) / MILLIS_TO_SEC)*SEC_TO_MIN};
+    delay(250);
+    // update counter when monitor finds that a beat has been measured
+    analog_beat_counter++;
+    float analog_interval_hr[analog_beat_counter] = {1./ ((currentAnalogMillis - previousTime) / MILLIS_TO_SEC)*SEC_TO_MIN};
+
+    // update instantaneous heart rate count when there are at least two beats identified
+    if (analog_beat_counter >= 2) {
+      analog_inst_hr_count++;
+      //calculate instantaneous heart rate
+      float analog_inst_hr[analog_inst_hr_count] = {(analog_interval_hr[1] + analog_interval_hr[2])/2};
+
+      //send the analog_inst_hr value to the analog_avg_hr array
+      analog_avg_hr.push(analog_inst_hr[analog_inst_hr_count]);
+      Serial.println(analog_inst_hr[analog_inst_hr_count]);
+    }
   
-  if (analog_beat_counter >= 2) {
-    analog_inst_hr_count++;
-    float analog_inst_hr[analog_inst_hr_count] = {(analog_interval_hr[1] + analog_interval_hr[2])/2};
-    analog_avg_hr.push(analog_inst_hr[analog_inst_hr_count]);
-  }
-  
-  previousTime = currentAnalogMillis;
+    previousTime = currentAnalogMillis;
     
   }
 
+  // if the current analog millis time is greater than one minute, begin calculating the rolling average
+  // currently updates at each minute. Can be updated to calculate it at each new beat
   if (currentAnalogMillis - startTime >= (MILLIS_TO_SEC * SEC_TO_MIN)) {
     float analog_avg_hr_val = analog_avg_hr.mean();
     startTime = currentAnalogMillis;
@@ -97,11 +116,7 @@ void readECGdigital() {
 
   
 }
-void readECGDigital() {
 
-
-  
-}
 
 // Still need to fix diagnosis to blink LEDs appropriately
 void diagnosis(float hrVal) {
@@ -134,5 +149,10 @@ void blinkLED(unsigned long previousMillis, unsigned long interval) {
     digitalWrite(diagnosticLED, ledState);
     
   }
+}
+
+void pause() {
+pauseState = ! pauseState;
+  
 }
 
