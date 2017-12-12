4e98cb529339f7fa490af30b581c5eef2c9b5029
Jawad Hoballah <jawad.hoballah@duke.edu>
2017-12-12 19:16:13 +0200

    Updating the analog_avg_hr array to not use a variable as the input for the array size for debugging purposes.

--- ECGMonitor.ino
+++ ECGMonitor.ino
@@ -1,11 +1,11 @@
 #include <Average.h>
 int brachyLim = 40;
 int tachyLim = 180;
-int rolling_average_size = 30;
-int MAXVOLT = 5;
-int MILLIS_TO_SEC = 1000;
-int SEC_TO_MIN = 60;
-int MIN_IN_MILLIS = 60000;
+const int rolling_average_size = 30;
+const int MAXVOLT = 5;
+const long MILLIS_TO_SEC = 1000;
+const int SEC_TO_MIN = 60;
+const long MIN_IN_MILLIS = 60000;
 
 int diagnosticLED = 12;
 int ecgAnalogSignalPin = A5;
@@ -32,7 +32,7 @@ const byte pausePin = 2;
 volatile byte pauseState = LOW;
 
 // holds the analog_avg_hr using the Average library
-Average<float> analog_avg_hr(rolling_average_size);
+Average<float> analog_avg_hr(30);
 
 
 void setup() {
